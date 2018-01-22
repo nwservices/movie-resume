79cfccd17f719b978953e7168b0c28135b42c9ec
Gilles Blanchard <neworldexport@gmail.com>
2018-01-22 12:38:35 +0100

    uploading files
    again

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// $(function()){
+
+	$('#butt').on("click",function(){
+		$('#container',).toggleClass('hovered'),
+		$('.container ').toggleClass('deblock');
+		setTimeout( function() {
+		  window.location.href='#ancreread';
+		}, 1150);
+	});
+
+// }
+
